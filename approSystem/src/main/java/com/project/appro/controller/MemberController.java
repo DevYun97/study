67a6b1f738358297e2dde592370dbf4dc58cf8d3
DevYun97 <yundh812@gmail.com>
2022-09-28 22:51:40 +0900

    빌더 적용

--- approSystem/src/main/java/com/project/appro/controller/MemberController.java
+++ approSystem/src/main/java/com/project/appro/controller/MemberController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -44,13 +45,9 @@ public class MemberController {
 	//신규 사원 발급
 	@RequestMapping("memberUpdate")
 	@ResponseBody
-	public String memberUpdate (
-			@RequestParam("member_id") String member_id,
-			@RequestParam("member_dep") String member_dep,
-			@RequestParam("member_position") String member_position
-			) {
-			
-		String result = memService.memberUpdate(member_id , member_dep, member_position);
+	public String memberUpdate (@ModelAttribute MemberDTO member) {
+		
+		String result = memService.memberUpdate(member);
 			
 		return result;
 	}
@@ -58,14 +55,9 @@ public class MemberController {
 	//신규 사원 발급
 	@RequestMapping("memberJoinAction")
 	@ResponseBody
-	public String memberJoinAction (
-			@RequestParam("member_name") String member_name,
-			@RequestParam("member_dep") String member_dep,
-			@RequestParam("member_position") String member_position,
-			@RequestParam("member_gender") String member_gender
-			) {
+	public String memberJoinAction (@ModelAttribute MemberDTO member) {
 		
-		String result = memService.memberJoin(member_name, member_dep, member_position, member_gender);
+		String result = memService.memberJoin(member);
 		
 		return result;
 	}
