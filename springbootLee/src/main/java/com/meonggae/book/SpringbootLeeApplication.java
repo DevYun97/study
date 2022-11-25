931f85c6eed4f3643ab55b36fe03c32aa52f74a5
DevYun97 <yundh812@gmail.com>
2022-11-25 16:43:01 +0900

    이동욱저자 설정

---
+++ springbootLee/src/main/java/com/meonggae/book/SpringbootLeeApplication.java
@@ -0,0 +1,13 @@
+package com.meonggae.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLeeApplication.class, args);
+	}
+
+}
