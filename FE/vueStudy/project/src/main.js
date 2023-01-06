70db8b31d97cfaebc754386b2b54b47bb5c4f281
DevYun97 <yundh812@gmail.com>
2023-01-06 17:53:22 +0900

    부트 및 설치

--- FE/vueStudy/project/src/main.js
+++ FE/vueStudy/project/src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// Import all plugins
+import * as bootstrap from 'bootstrap';
+
+// Or import only needed plugins
+import { Tooltip as Tooltip, Toast as Toast, Popover as Popover } from 'bootstrap';
+
+// Or import just one
+import Alert as Alert from '../node_modules/bootstrap/js/dist/alert';
 
 createApp(App).use(store).use(router).mount('#app')
--- FE/vueStudy/project/src/views/HomeView.vue
+++ FE/vueStudy/project/src/views/HomeView.vue
@@ -3,16 +3,19 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+  <HeaderTest />
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderTest from '@/components/Header.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    HeaderTest
   }
 }
 </script>
